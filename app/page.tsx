095d6339aad94b9d505c19de5b8c7a3dfe982ce1
Elijah Seed-Arita <elijaharita@gmail.com>
2025-05-21 22:36:32 -0700

    feat: redirect to latest experiment from main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import { redirect } from "next/navigation";
+
 export default function Page() {
-  return <div>Hello</div>;
+  redirect("/experiments/detachable-player");
+
+  return <div></div>;
 }
